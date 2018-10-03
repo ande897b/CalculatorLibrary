a6f81b24da0dfb4f3140606801692af96058aff7
Anders Kristian Pedersen <ande897b@edu.eal.dk>
2018-10-03 12:13:17 +0200

    ADD virker

--- CalculatorLibrary/Calculator.cs
+++ CalculatorLibrary/Calculator.cs
@@ -4,6 +4,44 @@ namespace CalculatorLibrary
 {
     public class Calculator
     {
+        public static double Add(double v1, double v2)
+        {
+            return v1 + v2;
+        }
 
+        public static double Subtract(double v1, double v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Multiply(double v1, double v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Divide(double v1, double v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Sum(double[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Minimum(double[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Maximum(double[] numbers)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static double Average(double[] numbers)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
